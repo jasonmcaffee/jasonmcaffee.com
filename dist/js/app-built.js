e1064a5abee73873e583e8033da9bce36f3a9487
jasonmcaffee <jasonlmcaffee@gmail.com>
2013-10-04 20:50:53 -0400

    added some styling

--- dist/js/app-built.js
+++ dist/js/app-built.js
@@ -16324,7 +16324,7 @@ templates['threejsDemoOneTemplate'] = template(function (Handlebars,depth0,helpe
   var foundHelper, self=this;
 
 
-  return "<div id=\"demoOnePage\">\n    <div id=\"controls\">\n        <div id=\"moveForward\">Forward</div>\n        <div id=\"moveBackward\">Backward</div>\n        <div id=\"moveLeft\">Left</div>\n        <div id=\"moveRight\">Right</div>\n        <div id=\"moveUp\">Up</div>\n        <div id=\"moveDown\">Down</div>\n    </div>\n    <div id=\"scene\">\n\n    </div>\n</div>";}); 
+  return "<div id=\"demoOnePage\">\n    <div id=\"controls\">\n        <div id=\"moveForward\">Forward <span>w</span></div>\n        <div id=\"moveBackward\">Backward <span>s</span></div>\n        <div id=\"moveLeft\">Left <span>a</span></div>\n        <div id=\"moveRight\">Right <span>d</span></div>\n        <div id=\"moveUp\">Up <span>space</span></div>\n        <div id=\"moveDown\">Down <span>shift</span></div>\n    </div>\n    <div id=\"scene\">\n\n    </div>\n</div>";}); 
 Handlebars.registerPartial("threejsDemoOneTemplate", templates["threejsDemoOneTemplate"]); 
 return templates["threejsDemoOneTemplate"]; 
 });
